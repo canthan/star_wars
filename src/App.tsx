b4232c42668e4b0f1d385d31c865db1c4f60e62a
Andrzej Globisz <andrzej.globisz@gmail.com>
2022-02-17 14:39:09 +0100

    adding router and basic routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-function App() {
+import { Route, Routes } from 'react-router-dom';
+
+import { Details } from './Details';
+import { List } from './List';
+
+import './App.scss';
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <Routes>
+        <Route path="/" element={<List />} />
+        <Route path="details" element={<Details />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
